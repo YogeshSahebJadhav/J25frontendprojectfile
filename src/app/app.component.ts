fcbf380d85ce6b80bfc8580233007a80aaed407b
YogeshSahebJadhav <jadhavyogeshs008@gmail.com>
2023-09-09 15:55:56 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firstproject';
+}
+
+//wecan also write in this way
+//template urls:template:'<h2>my basic<mark>project',
+//style urls:['h2{text-align:center;color:blue}']
